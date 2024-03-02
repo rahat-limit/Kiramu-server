cde45a5dbc04767239d92838e64d1835dd27be37
Zhaishylyk Rakhat <76480155+rahat-limit@users.noreply.github.com>
2024-03-02 11:11:40 +0600

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ['http://localhost:3000'],
+    origin: ['*'],
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   }),
